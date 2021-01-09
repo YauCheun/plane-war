716958afd1371651573e78441583a94ed1d43fd3
YauCheun <1272125039@qq.com>
2021-01-09 21:08:47 +0800

    完善render函数业务逻辑和webpack

--- src/runtime-canvas/index.js
+++ src/runtime-canvas/index.js
@@ -22,12 +22,36 @@ const render = createRenderer({
   insert(el,parent){
     parent.addChild(el)
   },
+  //渲染函数第二个参数接受props
   patchProp(el,key,prevValue,nextValue){
     el[key]=nextValue
   },
+  //设置文本内容
   setElementText(node,text){
     const cText = new Text(text)
     node.addChild(cText)
+  },
+  // createText(text){
+  //   return new Text(text)
+  // },
+  //处理注释
+  createComment(){
+
+  },
+  // 获取父节点
+  parentNode(){
+
+  },
+  //获取兄弟节点
+  nextSibling(){
+
+  },
+  //删除节点
+  remove(el){
+    const parent = el.parent
+    if(parent){
+      parent.removeChild(el)
+    }
   }
 })
 export function createApp(rootComponent){
--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,19 @@ module.exports = {
   devtool: "source-map",
   devServer:{
     contentBase: path.resolve(__dirname,"./dist")
+  },
+  module: {
+    rules: {
+      test: /\.(png|jpe?g|gif)$/i,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            outputPath: "assets/",
+            publicPath: ""
+          }
+        }
+      ]
+    }
   }
 }
